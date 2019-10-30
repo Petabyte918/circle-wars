5623a6149ce3847b129abc47940c57e52b2e6bfd
Aaron Gayle <agayle@outlook.com>
2019-10-30 14:49:03 -0500

    fixing timers.js >.<

--- server/components/generic/Timers.js
+++ server/components/generic/Timers.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 export class Timers {
     name = 'timers'
     value = []
-=======
-export class Timers {
-    name = 'timers'
-    value = []
->>>>>>> 7e5b736f85d4a373a4c5e052b5ddce682d9adfb8
 }
