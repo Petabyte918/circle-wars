c82e6be343711a7c97dfd611d3d395b06a29d569
Aaron Gayle <agayle@outlook.com>
2019-10-30 16:46:25 -0500

    debugging

--- server/systems/Spellcaster.js
+++ server/systems/Spellcaster.js
@@ -6,11 +6,12 @@ export class SpellcasterSystem {
         for (let entityKey in entities) {
             entities[entityKey].components.player.spellcastPending = false
             if (!entities[entityKey].components.spellbook ||
-                !entities[entityKey].components.spellbook[entities[entityKey].components.player.spellcastPending - 1] ||
-                !entities[entityKey].components.spellbook[entities[entityKey].components.player.spellcastPending - 1].castable) {
-                console.log(entities[entityKey].components.spellbook)
+                !entities[entityKey].components.spellbook.value[entities[entityKey].components.player.spellcastPending - 1] ||
+                !entities[entityKey].components.spellbook.value[entities[entityKey].components.player.spellcastPending - 1].castable) {
                 return console.log(`spell doesn't exist or isn't castable`)
             }
+            const spell = entities[entityKey].components.spellbook.value[entities[entityKey].components.player.spellcastPending - 1]
+            return console.log(`entity ${entityKey} is casting spell ${spell.spellName}`)
 
         }
     }
