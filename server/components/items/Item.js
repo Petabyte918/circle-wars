9de3db39ec8aee7f14ef48aaf891fc50b3c6aa47
Aaron Gayle <agayle@outlook.com>
2019-10-30 14:56:35 -0500

    removing yarn \n fixing item.js

--- server/components/items/Item.js
+++ server/components/items/Item.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 export class Item {
     name = 'item'
     tradable
     constructor(tradable) {
         this.tradable = tradable
     }
-=======
-export class Item {
-    name = 'item'
-    tradable
-    constructor(tradable) {
-        this.tradable = tradable
-    }
->>>>>>> 7e5b736f85d4a373a4c5e052b5ddce682d9adfb8
 }
