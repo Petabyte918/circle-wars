422abf48a95c29bf84a9700339e6dc176a1802f9
Aaron Gayle <agayle@outlook.com>
2019-10-30 14:47:13 -0500

    fixing timer.js

--- server/components/generic/Timer.js
+++ server/components/generic/Timer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export class Timer {
     name = 'timer'
     start
@@ -7,14 +6,4 @@ export class Timer {
         this.start = time
         this.value = time
     }
-=======
-export class Timer {
-    name = 'timer'
-    start
-    value
-    constructor(time) {
-        this.start = time
-        this.value = time
-    }
->>>>>>> 7e5b736f85d4a373a4c5e052b5ddce682d9adfb8
 }
