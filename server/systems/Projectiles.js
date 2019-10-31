cb5bc483da913f41b4ca549f1f57765b991a093f
Aaron Gayle <agayle@outlook.com>
2019-10-30 22:34:33 -0500

    debugging

--- server/systems/Projectiles.js
+++ server/systems/Projectiles.js
@@ -3,8 +3,8 @@ export class ProjectilesSystem {
     constructor() { }
     step(_entities) {
         const entities = pick(_entities, e => e.components.type && e.components.type.value === 'projectile')
-        entities.forEach(projectile => {
-            //const velocity = 
-        })
+        //entities.forEach(projectile => {
+        //const velocity = 
+        //})
     }
 }
