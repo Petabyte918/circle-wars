608c9d3f9a0d243ee83a65ec081f28429b3487dc
Aaron Gayle <agayle@outlook.com>
2019-11-01 16:11:48 -0500

    updating

--- server/prefabs/spells/FrostBolt.js
+++ server/prefabs/spells/FrostBolt.js
@@ -2,6 +2,7 @@ import { Spell } from "../../components/combat/spells/Spell"
 import { OnKeyDown } from "../../components/combat/spells/events/OnKeyDown"
 import { OnFinishCasting } from "../../components/combat/spells/events/OnFinishCasting"
 import { BeginCasting } from "../../components/combat/spells/actions/BeginCasting"
+import { ChargeMana } from "../../components/combat/spells/actions/ChargeMana"
 import { CreateProjectile } from "../../components/combat/spells/actions/CreateProjectile"
 
 export class FrostBolt extends Spell {
