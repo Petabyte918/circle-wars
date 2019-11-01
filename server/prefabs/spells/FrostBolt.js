f474b7aef0747c7e1c862c50340b7b5323c21fb8
Aaron Gayle <agayle@outlook.com>
2019-11-01 10:51:27 -0500

    updating

--- server/prefabs/spells/FrostBolt.js
+++ server/prefabs/spells/FrostBolt.js
@@ -1,8 +1,11 @@
-import { CastTimeBehavior } from "../../components/combat/spells/behaviors/CastTime"
+import { OnKeyDown } from "../../components/combat/spells/events/OnKeyDown"
+import { BeginCasting } from "../../components/combat/spells/actions/BeginCasting"
+import { OnFinishCasting } from "../../components/combat/spells/events/OnFinishCasting"
 
 export class FrostBolt extends Spell {
     constructor() {
-        super("Frost Bolt", true)
-        this.addBehavior(new CastTimeBehavior(2000))
+        super("Frost Bolt")
+        this.addEvent(new OnKeyDown(new BeginCasting(2000)))
+            .addEvent(new OnFinishCasting(new CreateProjectile(2)))
     }
 }
