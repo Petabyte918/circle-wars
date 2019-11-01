543cea605b233dc241b22e1a5432565a9163939d
Aaron Gayle <agayle@outlook.com>
2019-11-01 11:11:47 -0500

    updating

--- server/systems/Spellcaster.js
+++ server/systems/Spellcaster.js
@@ -7,20 +7,18 @@ export class SpellcasterSystem {
             const entity = entities[entityKey]
             if (entity.components.caster.spellKeyDown) {
                 const spellIndex = entity.components.caster.spellKeyDown - 1
-                entity.components.caster.spellKeyDown = false
+                entities[entityKey].components.caster.spellKeyDown = false
                 if (!entity.components.spellbook.value[spellIndex]) return
                 if (entity.components.spellbook.value[spellIndex].events.onKeyDown) {
                     console.log(`handle actions ${entity.components.spellbook.value[spellIndex].events.onKeyDown}`)
-                    entity.components.caster.spellKeyDown = false
                 }
             }
             if (entity.components.caster.spellKeyUp) {
                 const spellIndex = entity.components.caster.spellKeyUp - 1
-                entity.components.caster.spellKeyUp = false
+                entities[entityKey].components.caster.spellKeyUp = false
                 if (!entity.components.spellbook.value[spellIndex]) return
                 if (entity.components.spellbook.value[spellIndex].events.onKeyUp) {
                     console.log(`handle actions ${entity.components.spellbook.value[spellIndex].events.onKeyUp}`)
-                    entity.components.caster.spellKeyUp = false
                 }
             }
         }
