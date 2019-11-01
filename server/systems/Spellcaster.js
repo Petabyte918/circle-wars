f56bbdc8ed6b4f02124c13d06b05ac31a0912536
Aaron Gayle <agayle@outlook.com>
2019-11-01 11:18:14 -0500

    updating

--- server/systems/Spellcaster.js
+++ server/systems/Spellcaster.js
@@ -7,18 +7,18 @@ export class SpellcasterSystem {
             const entity = entities[entityKey]
             if (entity.components.caster.spellKeyDown) {
                 const spellIndex = entity.components.caster.spellKeyDown - 1
-                entities[entityKey].components.caster.spellKeyDown = false
+                entity.components.caster.spellKeyDown = false
                 if (!entity.components.spellbook.value[spellIndex]) return
                 if (entity.components.spellbook.value[spellIndex].events.onKeyDown) {
-                    console.log(`handle actions ${entity.components.spellbook.value[spellIndex].events.onKeyDown}`)
+                    console.log(`handle actions ${entity.components.spellbook.value[spellIndex].events.onKeyDown.actions}`)
                 }
             }
             if (entity.components.caster.spellKeyUp) {
                 const spellIndex = entity.components.caster.spellKeyUp - 1
-                entities[entityKey].components.caster.spellKeyUp = false
+                entity.components.caster.spellKeyUp = false
                 if (!entity.components.spellbook.value[spellIndex]) return
                 if (entity.components.spellbook.value[spellIndex].events.onKeyUp) {
-                    console.log(`handle actions ${entity.components.spellbook.value[spellIndex].events.onKeyUp}`)
+                    console.log(`handle actions ${entity.components.spellbook.value[spellIndex].events.onKeyUp.actions}`)
                 }
             }
         }
