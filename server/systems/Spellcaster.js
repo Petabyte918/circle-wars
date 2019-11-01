39cf8593c908b84c5058f553bd561580a4177e0e
Aaron Gayle <agayle@outlook.com>
2019-11-01 10:56:13 -0500

    updating

--- server/systems/Spellcaster.js
+++ server/systems/Spellcaster.js
@@ -5,15 +5,14 @@ export class SpellcasterSystem {
         const entities = pick(_entities, e => e.components.caster && (e.components.caster.spellKeyDown || e.components.caster.spellKeyUp))
         for (let entityKey in entities) {
             const entity = entities[entityKey]
-            console.log(entity.components.caster.spellKeyDown, entity.components.spellbook.value)
             if (entity.components.caster.spellKeyDown) {
-                if (entity.components.spellbook.value[entity.components.caster.spellKeyDown].events.onKeyDown) {
+                if (entity.components.spellbook.value[entity.components.caster.spellKeyDown - 1].events.onKeyDown) {
                     console.log(`handle actions ${entity.components.spellbook.value[entity.components.caster.spellKeyDown].events.onKeyDown}`)
                     entity.components.caster.spellKeyDown = false
                 }
             }
             if (entity.components.caster.spellKeyUp) {
-                if (entity.components.spellbook.value[entity.components.caster.spellKeyUp].events.onKeyUp) {
+                if (entity.components.spellbook.value[entity.components.caster.spellKeyUp - 1].events.onKeyUp) {
                     console.log(`handle actions ${entity.components.spellbook.value[entity.components.caster.spellKeyUp].events.onKeyUp}`)
                     entity.components.caster.spellKeyUp = false
                 }
